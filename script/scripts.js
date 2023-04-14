8d9c87654299fa2bb1c9a58fb9d79ac8ad910629
Christopher <127637098+Krisstopher15@users.noreply.github.com>
2023-04-14 04:19:17 -0400

    Calculator script

---
+++ script/scripts.js
@@ -0,0 +1,29 @@
+const buttons = document.querySelectorAll("button");
+const display = document.querySelector(".display");
+
+let calculation = [];
+let displayCalculation;
+const getValue = (button) => {
+  const value = button.textContent;
+
+  if (value == "C") {
+    calculation = [];
+    display.textContent = "0";
+  } else if (value == "=") {
+    display.textContent = eval(displayCalculation);
+  } else if (value == "%") {
+    display.textContent = displayCalculation / 100;
+  } else {
+    calculation.push(value);
+    displayCalculation = calculation.join("");
+    display.textContent = displayCalculation;
+  }
+  if (displayCalculation > 9999999) {
+    display.textContent = "ERROR";
+    calculation = [];
+  }
+};
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => getValue(button));
+});
